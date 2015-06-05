f7b300f8aa873ce3d7b8018ecb79cfaf2a5b2557
Joseph Jordan <jcjordn5@gmail.com>
2015-06-04 21:19:22 -0400

    Winner function now reports ties

--- tictactoe.js
+++ tictactoe.js
@@ -22,36 +22,47 @@ var game = {
       this.player = player2;
     }
     else {
-      $(event.target).closest('div').append('<div class="knotBox">' + 'O' + '</div>');
+      $(event.target).closest('div').append('<div class="knotBox">' + 'O' + '</div>')
+      ;
       this.checkWinner();
       this.player = player1;
+
     }
+
   },
  checkWinner: function () {
-   if ((this.cells[0][0].text() === "X" && this.cells[0][1].text() === "X" && this.cells[0][2].text() ==="X") ||
-      (this.cells[1][0].text() === "X" && this.cells[1][1].text() === "X" && this.cells[1][2].text() ==="X") ||
-      (this.cells[2][0].text() === "X" && this.cells[2][1].text() === "X" && this.cells[2][2].text() ==="X") ||
-      (this.cells[0][0].text() === "X" && this.cells[1][0].text() === "X" && this.cells[2][0].text() ==="X") ||
-      (this.cells[0][1].text() === "X" && this.cells[1][1].text() === "X" && this.cells[2][1].text() ==="X") ||
-      (this.cells[0][2].text() === "X" && this.cells[1][2].text() === "X" && this.cells[2][2].text() ==="X") ||
-      (this.cells[0][0].text() === "X" && this.cells[1][1].text() === "X" && this.cells[2][2].text() ==="X") ||
-      (this.cells[0][2].text() === "X" && this.cells[1][1].text() === "X" && this.cells[2][0].text() ==="X"))
-      {
-$("#board").append("<h2>PLAYER2 WINS!!!</h2>");
-      }
+
+   if ((this.cells[0][0].text() === this.cells[0][1].text() && this.cells[0][0].text() === this.cells[0][2].text() && (this.cells[0][0].text() === "X")) ||
+   (this.cells[1][0].text() === this.cells[1][1].text() && this.cells[1][0].text() === this.cells[1][2].text() && (this.cells[1][0].text() === "X")) ||
+   (this.cells[2][0].text() === this.cells[2][1].text() && this.cells[2][0].text() === this.cells[2][2].text() && (this.cells[2][0].text() === "X")) ||
+   (this.cells[0][0].text() === this.cells[1][0].text() && this.cells[0][0].text() === this.cells[2][0].text() && (this.cells[0][0].text() === "X")) ||
+   (this.cells[0][1].text() === this.cells[1][1].text() && this.cells[0][1].text() === this.cells[2][1].text() && (this.cells[0][1].text() === "X")) ||
+   (this.cells[0][2].text() === this.cells[1][2].text() && this.cells[0][2].text() === this.cells[2][2].text() && (this.cells[0][2].text() === "X")) ||
+   (this.cells[0][0].text() === this.cells[1][1].text() && this.cells[0][0].text() === this.cells[2][2].text() && (this.cells[0][0].text() === "X")) ||
+   (this.cells[0][2].text() === this.cells[1][1].text() && this.cells[0][2].text() === this.cells[2][0].text() && (this.cells[0][2].text() === "X"))){
+$("#board").append("<h2>PLAYER 2 WINS!!!</h2>");
+    }
     else if
-    ((this.cells[0][0].text() === "O" && this.cells[0][1].text() === "O" && this.cells[0][2].text() ==="O") ||
-       (this.cells[1][0].text() === "O" && this.cells[1][1].text() === "O" && this.cells[1][2].text() ==="O") ||
-       (this.cells[2][0].text() === "O" && this.cells[2][1].text() === "O" && this.cells[2][2].text() ==="O") ||
-       (this.cells[0][0].text() === "O" && this.cells[1][0].text() === "O" && this.cells[2][0].text() ==="O") ||
-       (this.cells[0][1].text() === "O" && this.cells[1][1].text() === "O" && this.cells[2][1].text() ==="O") ||
-       (this.cells[0][2].text() === "O" && this.cells[1][2].text() === "O" && this.cells[2][2].text() ==="O") ||
-       (this.cells[0][0].text() === "O" && this.cells[1][1].text() === "O" && this.cells[2][2].text() ==="O") ||
-       (this.cells[0][2].text() === "O" && this.cells[1][1].text() === "O" && this.cells[2][0].text() ==="O"))
-       {
-    $("#board").append("<h2>PLAYER1 WINS!!!</h2>");
-         }
-    },
+       ((this.cells[0][0].text() === this.cells[0][1].text() && this.cells[0][0].text() === this.cells[0][2].text() && (this.cells[0][0].text() === "O")) ||
+      (this.cells[1][0].text() === this.cells[1][1].text() && this.cells[1][0].text() === this.cells[1][2].text() && (this.cells[1][0].text() === "O")) ||
+      (this.cells[2][0].text() === this.cells[2][1].text() && this.cells[2][0].text() === this.cells[2][2].text() && (this.cells[2][0].text() === "O")) ||
+      (this.cells[0][0].text() === this.cells[1][0].text() && this.cells[0][0].text() === this.cells[2][0].text() && (this.cells[0][0].text() === "O")) ||
+      (this.cells[0][1].text() === this.cells[1][1].text() && this.cells[0][1].text() === this.cells[2][1].text() && (this.cells[0][1].text() === "O")) ||
+      (this.cells[0][2].text() === this.cells[1][2].text() && this.cells[0][2].text() === this.cells[2][2].text() && (this.cells[0][2].text() === "O")) ||
+      (this.cells[0][0].text() === this.cells[1][1].text() && this.cells[0][0].text() === this.cells[2][2].text() && (this.cells[0][0].text() === "O")) ||
+      (this.cells[0][2].text() === this.cells[1][1].text() && this.cells[0][2].text() === this.cells[2][0].text() && (this.cells[0][2].text() === "O"))){
+      $("#board").append("<h2>PLAYER 1 WINS!!!</h2>");
+    }
+
+   else if (((this.cells[0][0].text() === "X") || (this.cells[0][0].text() === "O")) && ((this.cells[1][0].text() === "X") || (this.cells[1][0].text() ===
+   "O")) && ((this.cells[2][0].text() === "X") || (this.cells[2][0].text() === "O")) && ((this.cells[0][1].text() === "X") || (this.cells[0][1].text()
+   === "O")) && ((this.cells[1][1].text() === "X") || (this.cells[1][1].text() === "O")) && ((this.cells[1][2].text() === "X") || (this.cells[1][2].text()
+   === "O"))
+   && ((this.cells[0][2].text() === "X") || (this.cells[0][2].text() === "O")) && ((this.cells[1][2].text() === "X") || (this.cells[1][2].text() === "O"))
+   && ((this.cells[2][2].text() === "X") || (this.cells[2][2].text() === "O"))){
+     $("#board").append("<h2>TIE!!!</h2>");
+   }
+ },
 
  tally: function () {
    player1.score++;
