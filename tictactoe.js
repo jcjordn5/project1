2be383c95d31b52b6f6caa8c1265c77b1f42c9e1
Joseph Jordan <jcjordn5@gmail.com>
2015-06-07 23:01:27 -0400

    added animated title screen plus altered the turn method in an attempt to ensure the reliability of altenating turns over the course of multiple plays

--- tictactoe.js
+++ tictactoe.js
@@ -11,30 +11,35 @@ var player2 = {
 };
 
 var game = {
-    player:player1,
-   gameOn:false,
+    player: player1,
+    curPlayer:0,
+   gameOn:true,
  cells: [[$("#cell1"), $("#cell4"), $("#cell7")],
           [$("#cell2"), $("#cell5"), $("#cell8")],
           [$("#cell3"), $("#cell6"), $("#cell9")]],
 
 run: function () {
-  if (player1.name !== "" && player2.name !== "") {
-    this.gameOn = true;
-    $("#board div:hover").css("background-color", "black;");
+  if (player1.name != "" && player2.name != "") {
+    $("#overlay").addClass("goup");
+    $("#overlay").css("height", "0%");
   }
+
+
 },
  //this method alternates turns by switching the player property and event listener everytime a grid box is appended
   turn: function () {
     if (this.gameOn){
-      if (this.player === player1) {
-        $(event.target).closest('div').html('<div class="crossBox">' + this.player.sign + '</div>');
-        this.checkWinner();
-        this.player = player2;
-      }
-      else {
+      if (this.curPlayer % 2 === 0) {
         $(event.target).closest('div').html('<div class="knotBox">' + this.player.sign + '</div>');
         this.checkWinner();
+        this.player = player2;
+        this.curPlayer++;
+      }
+      else {
+        $(event.target).closest('div').html('<div class="crossBox">' + this.player.sign + '</div>');
+        this.checkWinner();
         this.player = player1;
+        this.curPlayer++;
       }
     }
   },
@@ -49,6 +54,7 @@ run: function () {
    (this.cells[0][0].text() === this.cells[1][1].text() && this.cells[0][0].text() === this.cells[2][2].text() && (this.cells[0][0].text() === "X")) ||
    (this.cells[0][2].text() === this.cells[1][1].text() && this.cells[0][2].text() === this.cells[2][0].text() && (this.cells[0][2].text() === "X"))){
 $("#board").append("<h2 id='result'>" +  this.player.name.toUpperCase() +" WINS!!!</h2>");
+player2.score++;
 this.gameOn = false;
     }
     else if
@@ -61,6 +67,7 @@ this.gameOn = false;
       (this.cells[0][0].text() === this.cells[1][1].text() && this.cells[0][0].text() === this.cells[2][2].text() && (this.cells[0][0].text() === "O")) ||
       (this.cells[0][2].text() === this.cells[1][1].text() && this.cells[0][2].text() === this.cells[2][0].text() && (this.cells[0][2].text() === "O"))){
       $("#board").append("<h2 id='result'>" + this.player.name.toUpperCase() + " WINS!!!</h2>");
+      player1.score++;
       this.gameOn = false;
     }
 
@@ -100,17 +107,13 @@ this.gameOn = false;
 
 game.setListeners();
 $('#reset').on('click', function () {game.render()});
-$('#setName1').on('click', function () {
+$('#setNames').on('click', function () {
   $('#playerBoard').append('<h2 id="name1">' + $('#player1').val() + '</h2>');
   player1.name = $('#player1').val();
-  game.run();
-});
-$('#setName2').on('click', function () {
-  $('#playerBoard').append('<h2 id="name2">' + $('#player2').val() + '</h2>')
+  $('#playerBoard').append('<h2 id="name2">' + $('#player2').val() + '</h2>');
   player2.name = $('#player2').val();
   game.run();
 });
-$('#setName1').on('click', function () {$('#player1').remove()});
-$('#setName2').on('click', function () {$('#player2').remove()});
-$('#setName1').on('click', function () {$('#setName1').remove()});
-$('#setName2').on('click', function () {$('#setName2').remove()});
+$('#setNames').on('click', function () {$('#player1').remove()});
+$('#setNames').on('click', function () {$('#player2').remove()});
+$('#setNames').on('click', function () {$('#setNames').remove()});
